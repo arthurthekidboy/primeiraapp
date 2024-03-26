817787a4d0beaf085e150a6ed1a8c8ed54913951
Arthur das Neves Coutinho <arthurcoutinho600@gmail.com>
2024-03-26 09:13:52 -0300

    inicio dos comentarios

--- app/src/main/java/coutinho/neves/primeiraapp/MainActivity.java
+++ app/src/main/java/coutinho/neves/primeiraapp/MainActivity.java
@@ -7,16 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+//A Classe Main Activity é a classe principal do app//
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //O onCreate é metodo que esta chamado toda vez que a tela esta sendo criada//
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //O contentView cria os elementos de interface//
         setContentView(R.layout.activity_main);
+
+        //o button obtem o botão enviar atraves do Id//
+
         Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        // A ação a ser realizada ao pressionar o botão//
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            //o Onclick é executado quando clica o botão//
             public void onClick(View v) {
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
                 String textoDigitado = etDigiteAqui.getText().toString();
